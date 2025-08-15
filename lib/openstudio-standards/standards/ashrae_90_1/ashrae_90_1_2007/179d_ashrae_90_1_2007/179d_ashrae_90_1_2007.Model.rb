51e21de44ee888eea1961c625fb24fa491d37e65
JanghyunJK <janghyun.kim@nrel.gov>
2025-08-15 05:56:51 -0600

    changing output to additionalProperties

--- lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.Model.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2007/179d_ashrae_90_1_2007/179d_ashrae_90_1_2007.Model.rb
@@ -1153,7 +1153,6 @@ class ACM179dASHRAE9012007
   # only considering airloop outdoor airflow rates
   # and not considering packaged terminal outdoor airflow rates
   # @param model [object]
-  # @return [array] of airflow rates: [outdoor_airflow_rate_minimum_m_3_per_s, outdoor_airflow_rate_design_m_3_per_s]
   def get_minimum_and_design_outdoor_airflow_rates(model)
     outdoor_airflow_rate_minimum_m_3_per_s = 0.0
     outdoor_airflow_rate_design_m_3_per_s = 0.0
@@ -1208,7 +1207,8 @@ class ACM179dASHRAE9012007
       outdoor_airflow_rate_design_m_3_per_s += design_supply_oa_flow_rate
     end
 
-    return [outdoor_airflow_rate_minimum_m_3_per_s, outdoor_airflow_rate_design_m_3_per_s]
+    model.getBuilding.additionalProperties.setFeature('outdoor_airflow_rate_minimum_m_3_per_s', outdoor_airflow_rate_minimum_m_3_per_s)
+    model.getBuilding.additionalProperties.setFeature('outdoor_airflow_rate_design_m_3_per_s', outdoor_airflow_rate_design_m_3_per_s)
   end
 
   def mark_zone_dsoa_multiplier(model)
