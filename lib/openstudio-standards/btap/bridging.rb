82fe9f12af738a0c717a567611f33a2f35294891
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-11-26 20:58:24 +0000

    Fixed material quantities function to recognize the correct edge types

--- lib/openstudio-standards/btap/bridging.rb
+++ lib/openstudio-standards/btap/bridging.rb
@@ -1137,12 +1137,22 @@ module BTAP
       cp                  = CommonPaths.instance
       csv                 = CSV.read(cp.thermal_bridging_path, headers: true)
       material_quantities = {}
-      tally_edges         = @tally[:edges].transform_keys(&:to_s)
+
+      # The "convex/concave" suffix on tally edges can be safely ignored since
+      # they currently aren't supported by BTAP.
+      tally_edges = @tally[:edges].transform_keys{ |key| key.to_s.gsub(/concave|convex/, '') }
       tally_edges.each do |edge_type, value|
         value.each do |wall_reference_and_quality, quantity|
+
+          # Jamb, sill, and head may all be grouped under fenestration when
+          # referencing the thermal bridging CSV.
+          if edge_type == "jamb" or edge_type == "sill" or edge_type == "head" 
+            edge_type = "fenestration"
+          end
+
           result = csv.find do |row| 
-            row['construction_type'] == edge_type &&
-            row['wall_reference']    == wall_reference_and_quality
+            row["edge_type"]      == edge_type &&
+            row["wall_reference"] == wall_reference_and_quality
           end
 
           if result.nil?
