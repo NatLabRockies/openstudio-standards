115035e72ce6cd623b941d0be126084d9b2aac79
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-12-02 15:30:36 +0000

    Reverting hash helper file

--- lib/openstudio-standards/utilities/hash.rb
+++ lib/openstudio-standards/utilities/hash.rb
@@ -1,15 +1,9 @@
+# Add a "dig" method to Hash to check if deeply nested elements exist
+# From: http://stackoverflow.com/questions/1820451/ruby-style-how-to-check-whether-a-nested-hash-element-exists
 class Hash
-  # Add a "dig" method to Hash to check if deeply nested elements exist
-  # From: http://stackoverflow.com/questions/1820451/ruby-style-how-to-check-whether-a-nested-hash-element-exists
   def dig(*path)
     path.inject(self) do |location, key|
       location.respond_to?(:keys) ? location[key] : nil
-    end
-  end
-
-  def deeply_symbolize_keys
-    transform_keys(&:to_sym).transform_values do |value|
-      value.is_a?(Hash) ? value.deeply_symbolize_keys : value
     end
   end
 end
