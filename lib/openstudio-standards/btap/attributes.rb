cfebc29e8392f3b96937d10456ef5d6959a55251
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-12-02 19:26:55 +0000

    Documenting and rearranging `attributes.rb` to separate construction retrieval for branching between TBD constructions and legacy ones, work in progress

--- lib/openstudio-standards/btap/attributes.rb
+++ lib/openstudio-standards/btap/attributes.rb
@@ -1,9 +1,14 @@
-# BTAP Attributes: Currently stores model attributes related to envelopes. 
+# This file extends some of the OpenStudio classes to add some new methods
+# and parameters for convenience. Additionally, it also houses methods
+# involving the pre-processing of an OpenStudio model for shared use in one
+# of BTAP's facilities, for now BTAP Costing and BTAP Carbon. Currently,
+# this pre-processing only involves retrieving the correct constructions for
+# envelopes.
+
 require "openstudio"
 
 module BTAP
 
-  # Class modifications to simplyify post-analysis functions
   class OpenStudio::Model::Model
     def getThermalZonesSorted
       return @zones_sorted
@@ -19,7 +24,7 @@ module BTAP
       return @spaces_sorted
     end
 
-    def <<(space)
+    def <<(space) # Override the append operator to compile the sorted spaces
       @spaces_sorted << space
     end
   end
@@ -37,12 +42,15 @@ module BTAP
     attr_reader :construction_hash # Same as the previous.
   end
 
+  # Class for accessing and pre-processing model attributes.
   class Attributes
     attr_reader :model
     attr_reader :zones
     attr_reader :spaces
     attr_reader :surface_types
     
+    # @param model    [OpenStudio::Model::Model]
+    # @param standard [Standard]
     def initialize(model, standard)
       @model             = model
       @standard          = standard
@@ -66,15 +74,97 @@ module BTAP
         "GroundContactFloor"
       ]
 
+      # Surface type map for converting between surface type strings and TBD
+      # `stypes`.
+      @surface_type_tbd_map = {
+        "ExteriorWall"       => :wall,
+        "ExteriorRoof"       => :roof,
+        "ExteriorFloor"      => :floor,
+        "GroundContactWall"  => :wall,
+        "GroundContactRoof"  => :roof,
+        "GroundContactFloor" => :floor
+      }
+
       @zones  = [] 
       @spaces = [] 
 
-      self.compile
+      self.compile_model
+      self.compile_constructions(not @standard.tbd.nil?)
     end
 
-    # Compile all the pertinent data into the data structures of this class 
-    # while also appending to the exisitng OpenStudio ones. 
-    def compile
+    # Compile all the constructions associated with each surface and subsurface.
+    #
+    # @param use_tbd [Bool] Use TBD takeoffs for surfaces where available.
+    def compile_constructions(use_tbd)
+      takeoffs = @standard.tbd.tally[:takeoffs]
+      @spaces.each do |space|
+        @surface_types.each do |surface_type|
+          space.surfaces_hash[surface_type].each do |surface|
+            if takeoffs.has_key?(surface) and @surface_type_tbd_map.has_key?(surface_type)
+              compile_construction_tbd(surface, @surface_type_tbd_map[surface_type])
+            else
+              puts "[BTAP Attributes] Surface takeoff for #{surface.handle} with surface type #{surface_type} unavailable, defaulting to legacy takeoff."
+              compile_construction(surface)
+            end
+          end
+        end
+      end
+    end
+
+    # Get the construction for a given surface using the TBD-defined takeoffs.
+    # 
+    # @param surface          [OpenStudio::Model::Surface]
+    # @param tbd_surface_type [String]
+    def compile_construction_tbd(surface, tbd_surface_type)
+      construction_set = @standard.tbd.costed_assembly(@standard,structure, , @standard.tbd.model[], :lp) # TODO: Need tbd.model[:perform] attribute in the cache
+      surface.instance_variable_set(:@construction_set, construction_set)
+    end
+
+    # Get the construction for a given surface using legacy takeoffs.
+    # 
+    # @param surface [OpenStudio::Model::Surface]
+    def compile_construction(surface)
+      construction_set = @costing_database["raw"]["construction_sets"].select { |data|
+        data["template"].to_s.gsub(/\s*/, "") == template_type               and
+        data["building_type"].to_s.downcase   == building_type.to_s.downcase and
+        data["space_type"].to_s.downcase      == space_type.to_s.downcase    and
+        data["min_stories"].to_i              <= num_of_above_ground_stories and
+        data["max_stories"].to_i              >= num_of_above_ground_stories
+      }.first
+      surface.instance_variable_set(:@construction_set, construction_set)
+
+      if construction_set.nil?
+        next
+      end
+
+      @surface_types.each do |surface_type|
+        space.surfaces_hash[surface_type].each do |surface|
+
+          # Search for a matching opaque or glazing construction and append the type to the hash.
+          construction_hash = @costing_database["raw"]["constructions_opaque"].find { |construction|
+            construction["construction_type_name"] == construction_set[surface_type]
+          }
+          if not construction_hash.nil?
+            construction_hash["type"] = "opaque"
+            surface.instance_variable_set(:@construction_hash, construction_hash)
+          else
+            construction_hash = @costing_database["raw"]["constructions_glazing"].find { |construction|
+              construction["construction_type_name"] == construction_set[surface_type]
+            }
+            if not construction_hash.nil?
+              construction_hash["type"] = "glazing"
+              surface.instance_variable_set(:@construction_hash, construction_hash)
+            end
+          end
+        end
+      end
+    end
+
+    # Compile all the pertinent OpenStudio-related data into the data structures
+    # of this class while also appending to the exisitng OpenStudio ones. This
+    # adds accessors for zones, spaces, and surfaces while keeping them sorted
+    # for future accesses.
+    def compile_model
       template_type = @standard.template
       num_of_above_ground_stories = @model.getBuilding.standardsNumberOfAboveGroundStories.to_i
       
@@ -87,24 +177,14 @@ module BTAP
         zone.instance_variable_set(:@spaces_sorted, [])
 
         zone.spaces.sort.each do |space|
-          if space.spaceType.empty? or space.spaceType.get.standardsSpaceType.empty? or space.spaceType.get.standardsBuildingType.empty?
-            raise ("standards Space type and building type is not defined for space:#{space.name.get}. Skipping this space.")
+          if space.spaceType.empty? or 
+             space.spaceType.get.standardsSpaceType.empty? or 
+             space.spaceType.get.standardsBuildingType.empty?
+            raise (
+              "standards Space type and building type is not defined for space:#{space.name.get}. Skipping this space.")
           end
           zone    << space
           @spaces << space
-
-          space_type    = space.spaceType.get.standardsSpaceType
-          building_type = space.spaceType.get.standardsBuildingType
-
-          # Compile a list of construction sets for each space.
-          construction_set = @costing_database["raw"]["construction_sets"].select { |data|
-            data["template"].to_s.gsub(/\s*/, "") == template_type               and
-            data["building_type"].to_s.downcase   == building_type.to_s.downcase and
-            data["space_type"].to_s.downcase      == space_type.to_s.downcase    and
-            data["min_stories"].to_i              <= num_of_above_ground_stories and
-            data["max_stories"].to_i              >= num_of_above_ground_stories
-          }.first
-          space.instance_variable_set(:@construction_set, construction_set)
           
           surfaces_hash = {}
 
@@ -133,32 +213,6 @@ module BTAP
           surfaces_hash["GroundContactFloor"] = BTAP::Geometry::Surfaces::filter_by_surface_types(ground_surfaces, "Floor").sort
 
           space.instance_variable_set(:@surfaces_hash, surfaces_hash)
-
-          if construction_set.nil?
-            next
-          end
-
-          @surface_types.each do |surface_type|
-            space.surfaces_hash[surface_type].each do |surface|
-
-              # Search for a matching opaque or glazing construction and append the type to the hash.
-              construction_hash = @costing_database["raw"]["constructions_opaque"].find { |construction|
-                construction["construction_type_name"] == construction_set[surface_type]
-              }
-              if not construction_hash.nil?
-                construction_hash["type"] = "opaque"
-                surface.instance_variable_set(:@construction_hash, construction_hash)
-              else
-                construction_hash = @costing_database["raw"]["constructions_glazing"].find { |construction|
-                  construction["construction_type_name"] == construction_set[surface_type]
-                }
-                if not construction_hash.nil?
-                  construction_hash["type"] = "glazing"
-                  surface.instance_variable_set(:@construction_hash, construction_hash)
-                end
-              end
-            end
-          end
         end
       end
     end
