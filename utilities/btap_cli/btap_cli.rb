3ff7d34988ad691450b45d70ab2f7d34842d85e2
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2026-07-14 15:56:32 +0000

    `btap_cli.rb`: Requiring `openstudio-standards.rb` library file instead of defunct `libs.rb` file, remoivng that file

--- utilities/btap_cli/btap_cli.rb
+++ utilities/btap_cli/btap_cli.rb
@@ -1,9 +1,11 @@
-require_relative './libs'
+require 'optparse'
+require_relative '../../lib/openstudio-standards'
 @argument = {}
-@options = {}
+@options  = {}
+
 # Default folders if not using s3
-@options[:input_folder] = File.join(__dir__, 'input')
-@options[:output_folder] = File.join(__dir__, 'output')
+@options[:input_folder]   = File.join(__dir__, 'input')
+@options[:output_folder]  = File.join(__dir__, 'output')
 @options[:weather_folder] = File.join(__dir__, 'weather')
 optparse = OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} -s NAME id ..."
@@ -11,13 +13,13 @@ optparse = OptionParser.new do |opts|
     puts opts
     exit
   end
-  opts.on('--input_path NAME', "Default is #{@options[:input_folder]}") { |s| @options[:input_folder] = s }
+  opts.on('--input_path NAME',  "Default is #{@options[:input_folder]}")   { |s| @options[:input_folder] = s }
   opts.on('--output_path NAME', "Default is #{@options[:output_folder]} ") { |s| @options[:output_folder] = s }
 end
 optparse.parse!
-BTAP::Datapoint.new(input_folder: @options[:input_folder],
-                  output_folder: @options[:output_folder],
-                  weather_folder: @options[:weather_folder])
+BTAP::Datapoint.new(input_folder:   @options[:input_folder],
+                    output_folder:  @options[:output_folder],
+                    weather_folder: @options[:weather_folder])
 
 # Example command using s3 urls for input/output
 # bundle exec ruby utilities/btap_cli/btap_cli.rb --input_path 's3://834599497928/test_analysis_new/test_analysis_id/input/02ade4c9-5c11-437b-9664-b28a8d5d9efb' --output_path 's3://834599497928/test_analysis_new/test_analysis_id/output/'
