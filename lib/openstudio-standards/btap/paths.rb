f618140c3d96c73f69bc49c1998e316ad457f48e
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2026-07-23 20:12:35 +0000

    `btap/common_resources/`: Renaming this to `common`, renaming some variables

--- lib/openstudio-standards/btap/paths.rb
+++ lib/openstudio-standards/btap/paths.rb
@@ -1,6 +1,6 @@
 # BTAP Paths
 #
-# File paths related to the common resources folder.
+# File paths related to the common folder.
 
 module BTAP
   module Paths
@@ -10,29 +10,30 @@ module BTAP
       attr_accessor :costs_path
       attr_accessor :costs_local_factors_path
     end
-    resources                 = Pathname(__dir__) / "common_resources"
-    @costs_path               = resources / "costs.csv"
-    @costs_local_factors_path = resources / "costs_local_factors.csv"
+    btap_common               = Pathname(__dir__) / "common"
+    necb_common               = Pathname(__dir__) / "common"
+    @costs_path               = btap_common / "costs.csv"
+    @costs_local_factors_path = btap_common / "costs_local_factors.csv"
 
     COSTING_DATA_PATHS = [
-      resources / "locations.csv",
-      resources / "materials_opaque.csv",
-      resources / "materials_glazing.csv",
-      resources / "lighting_sets.csv",
-      resources / "lighting.csv",
-      resources / "materials_lighting.csv",
-      resources / "hvac_vent_ahu.csv",
-      resources / "materials_hvac.csv"
+      btap_common / "locations.csv",
+      btap_common / "materials_opaque.csv",
+      btap_common / "materials_glazing.csv",
+      btap_common / "lighting_sets.csv",
+      btap_common / "lighting.csv",
+      btap_common / "materials_lighting.csv",
+      btap_common / "hvac_vent_ahu.csv",
+      btap_common / "materials_hvac.csv"
     ]
 
-    CONSTRUCTIONS_PATH       = resources / "constructions.json"
-    CARBON_OPAQUE_PATH       = resources / "carbon_opaque.csv"
-    CARBON_GLAZING_PATH      = resources / "carbon_glazing.csv"
-    CARBON_FRAME_PATH        = resources / "carbon_frame.csv"
-    THERMAL_BRIDGING_PATH    = resources / "thermal_bridging.csv"
-    MECH_SIZING_PATH         = resources / "mech_sizing.json"
-    NECB_BUILDING_TYPES_PATH = resources / "necb_building_types.csv"
-    NECB_SPACE_TYPES_PATH    = resources / "necb_space_types.csv"
-    NEB_PRICES_PATH          = resources / "neb_end_use_prices.csv"
+    CONSTRUCTIONS_PATH        = btap_common / "constructions.json"
+    CARBON_OPAQUE_PATH        = btap_common / "carbon_opaque.csv"
+    CARBON_GLAZING_PATH       = btap_common / "carbon_glazing.csv"
+    CARBON_FRAME_PATH         = btap_common / "carbon_frame.csv"
+    THERMAL_BRIDGING_PATH     = btap_common / "thermal_bridging.csv"
+    MECH_SIZING_PATH          = btap_common / "mech_sizing.json"
+    NECB_BUILDING_TYPES_PATH  = btap_common / "necb_building_types.csv"
+    NECB_SPACE_TYPES_PATH     = btap_common / "necb_space_types.csv"
+    NEB_PRICES_PATH           = btap_common / "neb_end_use_prices.csv"
   end
 end
