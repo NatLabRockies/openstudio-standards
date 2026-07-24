6b003b8cd31b0ca57503068c59d1116b49f38250
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2026-07-24 12:56:44 +0000

    `utilities/btap_cli/`: Cleaning up this directory

--- utilities/btap_cli/update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ -z "$STANDARDS_BRANCH" ]; then STANDARDS_BRANCH=nrcan; fi
-sed -i '/^.*standards.*$/d' Gemfile
-echo "gem 'openstudio-standards', :github => 'NREL/openstudio-standards', :branch => '$STANDARDS_BRANCH'" | tee -a Gemfile
-bundle update openstudio-standards
